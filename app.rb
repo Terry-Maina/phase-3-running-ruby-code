45abd5e88653519021df7f5fa3e49dd34059be06
Teresa <tenjerim@gmail.com>
2023-02-09 16:34:44 +0300

    completed

--- app.rb
+++ app.rb
@@ -21,3 +21,31 @@ def add num1, num2
 end
 
 puts add(1,3) + add(2,2)
+
+puts "Hello world!"
+puts "Hello world!"
+puts "Hello world!"
+puts "Hello world!"
+
+
+# app.rb
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
+
+# This is the equivalent of calling:
+# puts [1, 2, 3].inspect
+p [1, 2, 3]
+
+
+# app.rb
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# pp is for "pretty-printing" complex data such as nested arrays and hashes
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+#Test 1
+puts("Hello World!")
+
+#Test 2
+print("Pass this test, please.")
